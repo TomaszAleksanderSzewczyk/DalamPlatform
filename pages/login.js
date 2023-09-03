25d238a13d27ea77693455bf59257840b7396a32
Tomasz Aleksander Szewczyk <brutusek952@gmail.com>
2023-09-03 14:34:46 +0200

    ER-5 Login interface UX/UI changes

--- pages/login.js
+++ pages/login.js
@@ -6,14 +6,12 @@ import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
 import Link from "next/link";
 import Alert from "@mui/material/Alert";
+import Grid from "@mui/material/Grid";
 import { useRouter } from "next/router";
 import useUserData from "../hooks/useUser";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import withAuthStatus from "../hoc/withAuthStatus";
 import { useEffect, useState } from "react";
 
@@ -92,115 +90,71 @@ function Login() {
   };
 
   return (
-    <Container component="main" maxWidth="lg">
+    <Container component="main" maxWidth="xs">
       <CssBaseline />
-      <Grid container spacing={0}>
-        <Grid item xs={12} sm={6}>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              height: "100vh",
-              overflow: "hidden",
-            }}
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          {!isRegister ? "Sign In" : "Sign Up"}
+        </Typography>
+        {error && <Alert severity="error">{error}</Alert>}
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 3 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            value={password}
+            autoComplete="current-password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button
+            type="submit"
+            disabled={isLoading}
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2, bgcolor: "primary.main", color: "white" }}
           >
-            <video
-              autoPlay
-              muted
-              loop
-              controls
-              style={{
-                width: "100%",
-                maxWidth: "50%", // Dodaj tę linijkę, aby ograniczyć szerokość wideo do połowy strony
-                height: "100vh",
-                objectFit: "cover",
-                position: "fixed",
-                zIndex: "21",
-                left: "0",
-              }}
-            >
-              <source
-                src= "https://github.com/TomaszAleksanderSzewczyk/DalamPlatform/blob/main/public/video.mp4"
-                type="video/mp4"
-              />
-              Your browser does not support the video tag.
-            </video>
-          </Box>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <Box
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              height: "100vh",
-              overflow: "auto",
-            }}
-          >
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-              <LockOutlinedIcon />
-            </Avatar>
-            <Typography component="h1" variant="h5">
-              {!isRegister ? "Sign In" : "Sign Up"}
-            </Typography>
-            {error && <Alert severity="error">{error}</Alert>}
-            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                value={password}
-                autoComplete="current-password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              {!isRegister && (
-                <FormControlLabel
-                  control={<Checkbox value="remember" color="primary" />}
-                  label="Remember me"
-                />
-              )}
-              <Button
-                type="submit"
-                disabled={isLoading}
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                {isRegister ? "Sign Up" : "Sign In"}
-              </Button>
-              <Grid container>
-                <Grid item xs></Grid>
-                <Grid item>
-                  <Link href="#" variant="body2">
-                    <a onClick={handleSwap}>
-                      {isRegister
-                        ? "Have an account? Sign in"
-                        : "Don't have an account? Sign Up"}
-                    </a>
-                  </Link>
-                </Grid>
-              </Grid>
-            </Box>
-          </Box>
-        </Grid>
-      </Grid>
+            {isRegister ? "Sign Up" : "Sign In"}
+          </Button>
+          <Grid container>
+            <Grid item xs></Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                <a onClick={handleSwap}>
+                  {isRegister
+                    ? "Have an account? Sign in"
+                    : "Don't have an account? Sign Up"}
+                </a>
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
     </Container>
   );
 }
