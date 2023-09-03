0d1b83cb6b1231a3d160e174a48c82cad17ee869
Tomasz Aleksander Szewczyk <brutusek952@gmail.com>
2023-09-03 14:17:55 +0200

    ER-5 added video for login menu

--- pages/login.js
+++ pages/login.js
@@ -1,23 +1,21 @@
 import { getSession, signIn } from "next-auth/react";
-
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { useEffect, useState } from "react";
-import Alert from "@mui/material/Alert";
+import Grid from "@mui/material/Grid";
 import Link from "next/link";
+import Alert from "@mui/material/Alert";
 import { useRouter } from "next/router";
 import useUserData from "../hooks/useUser";
-import { TroubleshootTwoTone } from "@mui/icons-material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
 import withAuthStatus from "../hoc/withAuthStatus";
+import { useEffect, useState } from "react";
 
 async function createUser(email, password) {
   const response = await fetch("/api/auth/signup", {
@@ -39,8 +37,8 @@ async function createUser(email, password) {
 
 function Login() {
   const { refetch } = useUserData();
-  const [email, setEmail] = useState("slaczka.sebastian@gmail.com");
-  const [password, setPassword] = useState("test123");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [isRegister, setIsRegister] = useState(false);
@@ -64,16 +62,14 @@ function Login() {
     event.preventDefault();
     if (isRegister) {
       const result = await createUser(email, password);
-      console.log(result);
 
       if (result.error) {
         setError(result.error);
       } else {
-        alert('Successfully registered, you can log in')
+        alert("Successfully registered, you can log in");
       }
       return;
     }
-
 
     const result = await signIn("credentials", {
       redirect: false,
@@ -81,96 +77,130 @@ function Login() {
       password,
     });
     await refetch();
-    console.log('result', result);
-
 
     const userData = await fetch("/api/user").then((res) => res.json());
 
     if (!result.error) {
-      console.log(userData, 'userdata');
-      // set some auth state
-        if (!userData?.firstName) {
-          router.push("/credentials");
-        } else router.push("/profile");
+      if (!userData?.firstName) {
+        router.push("/credentials");
+      } else router.push("/profile");
     }
 
     if (result.error) {
       setError(result.error);
     }
-};
+  };
 
   return (
-    <Container component='main' maxWidth='xs'>
+    <Container component="main" maxWidth="lg">
       <CssBaseline />
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component='h1' variant='h5'>
-          {!isRegister ? "Sign In" : "Sign Up"}
-        </Typography>
-        {error && <Alert severity='error'>{error}</Alert>}
-        <Box component='form' onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin='normal'
-            required
-            fullWidth
-            id='email'
-            label='Email Address'
-            name='email'
-            autoComplete='email'
-            autoFocus
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <TextField
-            margin='normal'
-            required
-            fullWidth
-            name='password'
-            label='Password'
-            type='password'
-            id='password'
-            value={password}
-            autoComplete='current-password'
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          {!isRegister && (
-            <FormControlLabel
-              control={<Checkbox value='remember' color='primary' />}
-              label='Remember me'
-            />
-          )}
-          <Button
-            type='submit'
-            disabled={isLoading}
-            fullWidth
-            variant='contained'
-            sx={{ mt: 3, mb: 2 }}
+      <Grid container spacing={0}>
+        <Grid item xs={12} sm={6}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              height: "100vh",
+              overflow: "hidden",
+            }}
           >
-            {isRegister ? "Sign Up" : "Sign In"}
-          </Button>
-          <Grid container>
-            <Grid item xs></Grid>
-            <Grid item>
-              <Link href='#' variant='body2'>
-                <a onClick={handleSwap}>
-                  {isRegister
-                    ? "Have an account? Sign in"
-                    : "Don't have an account? Sign Up"}
-                </a>
-              </Link>
-            </Grid>
-          </Grid>
-        </Box>
-      </Box>
+            <video
+              autoPlay
+              muted
+              loop
+              controls
+              style={{
+                width: "100%",
+                maxWidth: "50%", // Dodaj tę linijkę, aby ograniczyć szerokość wideo do połowy strony
+                height: "100vh",
+                objectFit: "cover",
+                position: "fixed",
+                zIndex: "21",
+                left: "0",
+              }}
+            >
+              <source
+                src= "https://github.com/TomaszAleksanderSzewczyk/DalamPlatform/blob/main/public/video.mp4"
+                type="video/mp4"
+              />
+              Your browser does not support the video tag.
+            </video>
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              height: "100vh",
+              overflow: "auto",
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              {!isRegister ? "Sign In" : "Sign Up"}
+            </Typography>
+            {error && <Alert severity="error">{error}</Alert>}
+            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                value={password}
+                autoComplete="current-password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              {!isRegister && (
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+              )}
+              <Button
+                type="submit"
+                disabled={isLoading}
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                {isRegister ? "Sign Up" : "Sign In"}
+              </Button>
+              <Grid container>
+                <Grid item xs></Grid>
+                <Grid item>
+                  <Link href="#" variant="body2">
+                    <a onClick={handleSwap}>
+                      {isRegister
+                        ? "Have an account? Sign in"
+                        : "Don't have an account? Sign Up"}
+                    </a>
+                  </Link>
+                </Grid>
+              </Grid>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
